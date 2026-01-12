babc7b356df21e2355f257324b7a6dbdbe193759
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-01-12 19:03:51 -0300

    Class getters e setters

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,46 +2,40 @@ package entities;
 
 public class Product {
 	
-	private String name;
+	String name;
 	private double price;
 	private int quantity;
 	
-	public  Product() {
+	
+	public Product() {
 		
 	}
-	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
-     	this.quantity = quantity;
-		
+		this.quantity = quantity;
 	}
-	
+
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
-	public void SetName(String name) {
-		 this.name = name;
+	public void setName(String name) {
+		this.name = name;
 	}
-	
 	public double getPrice() {
 		return price;
 	}
-	
-	public void SetPrice(double price) {
+	public void setPrice(double price) {
 		this.price = price;
 	}
-	
 	public int getQuantity() {
 		return quantity;
 	}
-	
 	
 	public double totalValueInStock() {
 		return price * quantity;
