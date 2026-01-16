bfcbca03fa9d7b4b808cd77b22882a67e7b36c26
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-01-16 14:27:23 -0300

    Update Class

---
+++ src/course_object_oriented/ExerciseVetores2.java
@@ -0,0 +1,24 @@
+package course_object_oriented;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExerciseVetores2 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/entities/Price.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Price {
+	
+	private String name;
+	private double price;
+	
+	public Price (String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+}
