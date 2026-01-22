426aeee43abdd802b75c3579c957fb6163642eb9
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-01-22 14:29:39 -0300

    Update class

--- src/course_object_oriented/VectHeights.java
+++ src/course_object_oriented/VectHeights.java
@@ -14,11 +14,11 @@ public class VectHeights {
 		System.out.print("How many peoples will be typed: ");
 		int n = sc.nextInt();
 		
+		VectHeights[] vect = new VectHeights[n];
+		
 		String[] name = new String[n];
 		int[] age = new int[n];
 		double[] height = new double[n];
-		
-		Heights[] vect = new Heights[n];
 		
 		for (int i = 0; i < name.length; i++) {
 			System.out.println("Data of " + (i+1) + "a person");
@@ -33,6 +33,11 @@ public class VectHeights {
 			height[i] = sc.nextDouble();	
 		}
 		
+		
+		double sum = 0.0;
+		for (i = 0; i < vect.length; i++) {
+			sum += vect[i].getHeight();
+		}
 		
 		sc.close();
 	}
