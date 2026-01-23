2ba223de6c482343484aae9388d404b1a2b72c25
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-01-23 03:07:39 -0300

    Concluded exercise POO listas

--- src/course_object_oriented/VectHeights.java
+++ src/course_object_oriented/VectHeights.java
@@ -3,7 +3,6 @@ package course_object_oriented;
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Heights;
-
 public class VectHeights {
 
 	public static void main(String[] args) {
@@ -14,29 +13,47 @@ public class VectHeights {
 		System.out.print("How many peoples will be typed: ");
 		int n = sc.nextInt();
 		
-		VectHeights[] vect = new VectHeights[n];
+		Heights[] vect = new Heights[n];
 		
-		String[] name = new String[n];
-		int[] age = new int[n];
-		double[] height = new double[n];
-		
-		for (int i = 0; i < name.length; i++) {
+	
+		for (int i = 0; i < vect.length; i++) {
 			System.out.println("Data of " + (i+1) + "a person");
 			
 			System.out.print("Name: ");
-			name[i] = sc.next();
+			String name = sc.next();
 			
 			System.out.print("Age: ");
-			age[i] = sc.nextInt();
+			int age = sc.nextInt();
 			
 			System.out.print("height: ");
-			height[i] = sc.nextDouble();	
+			double height = sc.nextDouble();
+			
+			vect[i] = new Heights(name, age, height);
 		}
 		
-		
+		System.out.println();
 		double sum = 0.0;
-		for (i = 0; i < vect.length; i++) {
-			sum += vect[i].getHeight();
+		for (int i = 0; i < vect.length; i++) {
+			sum += vect[i].Getheight();
+		}
+		
+		double avg = sum / n;
+		System.out.printf("Average Height: %.2f\n ", avg);
+		
+		int count = 0;
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i].Getage() < 16 )
+				count++;			
+		}
+		
+		double percentage = ((double) count / vect.length) * 100.00;
+		System.out.printf("Percentage of peoples with under 16: %.1f\n ", percentage);
+		
+		for (int i = 0; i < vect.length; i++ ) {
+			if (vect[i].Getage() < 16)
+				System.out.println(vect[i].Getname());
+			
+				
 		}
 		
 		sc.close();
