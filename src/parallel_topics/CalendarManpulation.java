be7a80bb74c56fb89c9abcf340abf3b2228537d5
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-02-19 12:38:39 -0300

    Learn about Calendar

---
+++ src/parallel_topics/CalendarManpulation.java
@@ -0,0 +1,37 @@
+package parallel_topics;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarManpulation {
+
+	public static void main(String[] args)throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Date d = Date.from(Instant.parse("2026-02-19T12:27:07Z"));
+		
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		
+		System.out.println(sdf.format(d));
+		System.out.println("----------------------------");
+		
+		Calendar cal2 = Calendar.getInstance();
+		cal.setTime(d);
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = 1 + cal.get(Calendar.MONTH);
+		
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
+
+	}
+
+}
