8459b846247989788bc151937739cb5cc49c5f93
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-02-21 12:15:10 -0300

    Traning using Enums

---
+++ src/course_object_oriented/ProgramEnum.java
@@ -0,0 +1,10 @@
+package course_object_oriented;
+
+public class ProgramEnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/entities/Order_enum.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.Date;
+
+import Entities_enums.OrderStatus;
+
+public class Order_enum {
+	
+	private Integer id;
+	private Date moment;
+	private OrderStatus status;
+	
+	public Order_enum() {	
+	}
+
+	public Order_enum(Integer id, Date moment, OrderStatus status) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Date getMoment() {
+		return moment;
+ 	}
+	
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+	
+	public OrderStatus getStatus() {
+		return status;
+	}
+	
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+}
