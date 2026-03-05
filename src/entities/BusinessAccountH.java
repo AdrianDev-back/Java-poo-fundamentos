e62a4997d5167178c5b812173a8da0c027e770f8
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-03-05 14:33:04 -0300

    Initial class herança

---
+++ src/entities/BusinessAccountH.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class BusinessAccountH extends AccountH {
+	
+	private Double loanLimit;
+	
+	public BusinessAccountH() {
+		super();
+	}
+	
+	public BusinessAccountH(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+	
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+	
+	public void setLoanlimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if (amount <= loanLimit) {
+			deposit(amount);
+		}
+	}
+
+}
