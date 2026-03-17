2123d2a78f8b7ce0d561ce0b2d8600d52afb0322
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-03-17 13:05:51 -0300

    exercise about abstract

---
+++ src/entities/ShapeCircle.java
@@ -0,0 +1,32 @@
+package entities;
+
+import Entities_enums.Color;
+
+public class ShapeCircle extends Shape {
+	
+	private Double radius;
+	
+	public ShapeCircle() {
+		super();
+	}
+	
+	public ShapeCircle(Color color, Double radius) {
+		super(color);
+		this.radius = radius; 
+	}
+	
+	public Double getRadius() {
+		return radius;
+	}
+	
+	public void setRadius(Double Radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return  Math.PI * radius * radius;
+	}
+	
+
+}
