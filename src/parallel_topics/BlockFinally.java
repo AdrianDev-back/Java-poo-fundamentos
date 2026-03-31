e401de87b4f34f26216dd654dd3c026ea6708de0
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-03-31 12:22:17 -0300

    Block finally

---
+++ src/parallel_topics/BlockFinally.java
@@ -0,0 +1,30 @@
+package parallel_topics;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BlockFinally {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\Intel\\in.txt.txt");
+		Scanner sc = null;
+		try {
+		    sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
