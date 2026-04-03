7cf1edf0b20f7a2c62081de875c7771826b7ae0e
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-04-03 10:47:21 -0300

    Class about file - Great solution

---
+++ src/parallel_topics/FileReader_Buffered_Great.java
@@ -0,0 +1,28 @@
+package parallel_topics;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReader_Buffered_Great {
+
+	public static void main(String[] args) {
+		
+		String path = "C:\\Intel\\in.txt.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+	}
+
+}
+
