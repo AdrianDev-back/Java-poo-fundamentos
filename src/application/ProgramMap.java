ab11a7b96a0c6ad9b6a1fd082ad99c3fd835c79c
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-04-21 15:11:01 -0300

    Exercise about Map Concluded

---
+++ src/application/ProgramMap.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ProgramMap {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
+		
+		Map<String, Integer> votes = new HashMap<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				String name = fields[0];
+				int quantity = Integer.parseInt(fields[1]);
+				
+				if (votes.containsKey(name)) {
+					int total = votes.get(name);
+					votes.put(name, total + quantity);
+				} else {
+					votes.put(name, quantity);
+				}
+				line = br.readLine();
+				
+			}
+			
+		} catch (IOException e ) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		for (String key : votes.keySet()) {
+			System.out.println(key + "; " + votes.get(key));
+		}
+		
+
+	}
+
+}
