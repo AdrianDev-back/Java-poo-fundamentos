866a711a63f5c83cae93493f676bd2f7bad8ce73
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-04-30 13:36:52 -0300

    Exercise with Stream

---
+++ src/model/entities/ProductStream.java
@@ -0,0 +1,26 @@
+package model.entities;
+
+public class ProductStream {
+	
+	private String name;
+	private Double price;
+	
+	public ProductStream(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+	
+
+}
